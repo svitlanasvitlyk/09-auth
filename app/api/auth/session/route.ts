b6d2e6c0afa4a6f3da970c6a0c3c039229ce28af
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2026-01-04 18:36:48 +0100

    update utils

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import { api } from "../../api";
 import { parse } from "cookie";
 import { isAxiosError } from "axios";
-import { logErrorResponse } from "../../_utils/_utils";
+import { logErrorResponse } from "../../_utils/utils";
 
 
 
